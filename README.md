1a06590b40b8fe9251019c692892312b85671be2
Son Do <doson@1digital.vn>
2022-04-10 02:03:59 +0700

    add script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# auto-dark-mode-scripts
-Script auto turn on dark mode for Tinhte.vn and Facebook
+# Auto dark script
+
+Userscripts to add functionality auto turn on dark mode for [Tinhte](https://tinhte.vn/) and [Facebook](https://www.facebook.com/)
+
+## Installation
+1. Make sure you have user scripts enabled in your browser (these instructions refer to the latest versions of the browser):
+
+	* [Tampermonkey](https://www.tampermonkey.net/)
+	* [Violentmonkey](https://violentmonkey.github.io/get-it/)
+	* [Greasemonkey](https://addons.mozilla.org/firefox/addon/greasemonkey/)
+
+2. Get information or install:
+	* Install a script directly from GitHub by download script [here](https://github.com/gh-sondh/auto-dark-mode-script) and import to your userscript manager.
+	* Install a script from [GreasyFork](https://greasyfork.org/)
---
+++ auto-dark-mode.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        Auto Dark Mode
+// @namespace   sondh/auto-dark-script
+// @author      sondh
+// @description Script auto change dark mode for Tinhte and Facebook
+// @description:vi Script tự động bật chế độ tối cho Tinhte và Facebook
+// @grant       none
+// @match       *://tinhte.vn/*
+// @match       *://*.facebook.com/*
+// @version     1.1
+// @homepage    https://github.com/gh-sondh/auto-dark-mode-script
+// @supportURL  https://github.com/gh-sondh/auto-dark-mode-script/issues
+// @grant       GM_listValues
+// @grant       GM_addStyle
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @run-at      document-end
+// ==/UserScript==
+
+console.log('Auto Dark Mode init...');
+
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const switchMode = (mode) => {
+    let className = '';
+    const rootHtml = document.documentElement;
+    const currentHost = window.location.host;
+
+    switch (currentHost) {
+        case "tinhte.vn":
+            className = "dark";
+            break;
+
+        default:
+            className = "__fb-dark-mode";
+            break;
+    }
+
+    rootHtml.classList.toggle(className, mode);
+
+    // Listener event System change style Dark mode
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+        switchMode(e.matches);
+    });
+};
+
+// init
+switchMode(isDarkMode);
